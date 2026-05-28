64a53a79772c9372b492a03f6fe30f819855b224
KWH <kmwh030@gmail.com>
2026-05-28 21:05:18 +0900

    Initial commit

---
+++ src/main/java/io/github/kmwh/tradeos_backend/TradeosBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.kmwh.tradeos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/kmwh/tradeos_backend/TradeosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kmwh.tradeos_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
