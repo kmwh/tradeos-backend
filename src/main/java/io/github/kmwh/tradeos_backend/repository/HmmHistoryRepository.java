442085b736f708468dd222ffa3e34bd187b08bb2
KWH <kmwh030@gmail.com>
2026-05-29 08:45:23 +0900

    fix: HmmHistoryRepository ID 타입 버그 수정

--- src/main/java/io/github/kmwh/tradeos_backend/repository/HmmHistoryRepository.java
+++ src/main/java/io/github/kmwh/tradeos_backend/repository/HmmHistoryRepository.java
@@ -2,10 +2,9 @@ package io.github.kmwh.tradeos_backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.time.LocalDateTime;
 
 import io.github.kmwh.tradeos_backend.entity.HmmHistory;
 
 @Repository
-public interface HmmHistoryRepository extends JpaRepository<HmmHistory, LocalDateTime> {
+public interface HmmHistoryRepository extends JpaRepository<HmmHistory, Long> {
 }
