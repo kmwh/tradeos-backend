0cae40cd60a7542f7245c1e2a7c55fc78de8b18a
KWH <kmwh030@gmail.com>
2026-05-30 14:39:03 +0900

    fix: JournalListResponseDto에 exitTime, leverage 추가

--- src/main/java/io/github/kmwh/tradeos_backend/journal/dto/JournalListResponseDto.java
+++ src/main/java/io/github/kmwh/tradeos_backend/journal/dto/JournalListResponseDto.java
@@ -3,10 +3,10 @@ package io.github.kmwh.tradeos_backend.journal.dto;
 import java.time.LocalDateTime;
 import io.github.kmwh.tradeos_backend.journal.entity.Journal;
 
-public record JournalListResponseDto(Long journalId, LocalDateTime entryTime, String position,
-    Double roi, Boolean isAnalyzed) {
+public record JournalListResponseDto(Long journalId, LocalDateTime entryTime,
+    LocalDateTime exitTime, String position, Double leverage, Double roi, Boolean isAnalyzed) {
   public JournalListResponseDto(Journal journal) {
-    this(journal.getId(), journal.getEntryTime(), journal.getPosition(), journal.getRoi(),
-        journal.getIsAnalyzed());
+    this(journal.getId(), journal.getEntryTime(), journal.getExitTime(), journal.getPosition(),
+        journal.getLeverage(), journal.getRoi(), journal.getIsAnalyzed());
   }
 }
