5f753b407a5490ac6466745d491d8ceac9967dd5
KWH <kmwh030@gmail.com>
2026-06-03 19:33:43 +0900

    fix: hmm 스케쥴러 4시간 간격으로 수정

--- src/main/java/io/github/kmwh/tradeos_backend/quant/scheduler/HmmScheduler.java
+++ src/main/java/io/github/kmwh/tradeos_backend/quant/scheduler/HmmScheduler.java
@@ -14,10 +14,7 @@ public class HmmScheduler {
   private final HmmPipelineService hmmPipelineService;
 
   // 4시간마다 실행
-  // @Scheduled(cron = "0 0 0,4,8,12,16,20 * * *")
-
-  // 테스트로 1분마다 실행
-  @Scheduled(cron = "0 * * * * *")
+  @Scheduled(cron = "0 0 0,4,8,12,16,20 * * *")
   public void runHmmPipeline() {
     log.info("===== HMM 데이터 수집 스케줄러 작동 시작 =====");
     hmmPipelineService.fetchAndSaveHmmScore();
